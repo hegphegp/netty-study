6f80b33fe8228ff7e424528fafa3f7388e8622f6
heguopei <heguopei@ascs.tech>
2018-07-12 18:02:49 +0800

    服务器添加netty服务启动报错的监听器，监听器添加一些处理逻辑

--- src/main/java/com/hegp/netty/basic/example03/server/SocketServer.java
+++ src/main/java/com/hegp/netty/basic/example03/server/SocketServer.java
@@ -4,17 +4,19 @@ import com.hegp.netty.basic.example03.common.decoder.MessageDecoder;
 import com.hegp.netty.basic.example03.common.encoder.MessageEncoder;
 import com.hegp.netty.basic.example03.server.handler.ServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
 
 public class SocketServer {
+
+    protected static Logger logger = LoggerFactory.getLogger(SocketServer.class);
 
     private ServerBootstrap bootstrap;
     private EventLoopGroup bossGroup;
@@ -32,24 +34,32 @@ public class SocketServer {
             bossGroup = new NioEventLoopGroup();
             bootstrap = new ServerBootstrap();
             bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 1024)
-//                    .option(ChannelOption.SO_SNDBUF, 32 * 1024) // 设置发送缓冲大小
-//                    .option(ChannelOption.SO_RCVBUF, 32 * 1024) // 这是接收缓冲大小
-                    .localAddress(port) //server监听端口
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline()
-                                .addLast("encoder", new MessageEncoder())
-                                //  1<<20 是 1024*1024
-                                .addLast("decoder", new MessageDecoder(1<<20, 10, 4))
-                                .addLast(new ServerHandler());
-                        }
-                    });
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+//                .option(ChannelOption.SO_SNDBUF, 32 * 1024) // 设置发送缓冲大小
+//                .option(ChannelOption.SO_RCVBUF, 32 * 1024) // 这是接收缓冲大小
+                .localAddress(port) //server监听端口
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline()
+                          .addLast("encoder", new MessageEncoder())
+                          //  1<<20 是 32
+                          .addLast("decoder", new MessageDecoder(1<<20, 10, 4))
+                          .addLast(new ServerHandler());
+                    }
+                });
             channelFuture = bootstrap.bind().sync();
-            channelFuture.channel().closeFuture().sync();
+            channelFuture.channel().closeFuture()
+                    .addListener(new ChannelFutureListener() {
+                        @Override
+                        public void operationComplete(ChannelFuture future) throws Exception {
+                            logger.error("server closed");
+                            // 服务器启动失败的一些逻辑处理
+                        }
+                    })
+                    .sync();
         } finally { // 资源优雅释放
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
